3457f7609002bc584a3980c608686303bc1da6ca
jmartinezrico <jocelyn.martinezrico.489@my.csun.edu>
2022-02-27 17:05:34 -0800

    oh look more edits

--- Tidy_Tuesday_1/scripts/tidy_script_1.R
+++ Tidy_Tuesday_1/scripts/tidy_script_1.R
@@ -29,23 +29,22 @@ Freedom <- FreeData %>% #change data script to view isolated variables properly
 view(Freedom)
 
 Freedom %>%
-  ggplot(data=Freedom, mapping = aes(x = year, y = PR,
-             group = country, 
-             color = country)) +
+  ggplot(data = Freedom, mapping = aes(x = year, y = PR,
+             group = country,
+             fill = country)) +
   geom_violin()+ #graph used to view distribution 
+  scale_fill_manual(values = beyonce_palette(129))+ #adds color
   labs(x = "Year", #defines x axis
        y = "Political Rights", #defines y axis
        title = "Political Rights from 2000s to Present Day", #adds title
        subtitle = "Data in regards to Colombia, Mexico, & USA", #adds subtitle
-       caption = "Source: TidyTuesday Data from 2022-02-22")+ #adds caption
-  theme_bw()+ #makes background white
-  scale_fill_manual(values = beyonce_palette(127))+ #adds color
+       caption = "Source: 2022-02-22 TidyTuesday Data")+ #adds caption
+  theme_bw()+ #makes background white and adds grid lines
   theme(axis.title = element_text(size = 15), #changes axis title size
         plot.title = element_text(size = 20), #changes title size
         text = element_text(family = "baskerville")) #adds font
 
-ggsave(here('Tidy_Tuesday_1/output',"Freedom_CMU_plot.png"), width = 7, height = 6)
-
+ggsave(here('Tidy_Tuesday_1/output',"Free_CMU_plot.png"), width = 7, height = 6)
 
 
 
