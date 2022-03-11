912d0be371887528b1382cb5c67d43bde3edc375
jmartinezrico <jocelyn.martinezrico.489@my.csun.edu>
2022-03-10 19:45:36 -0800

    tidy tuesday #3 final

--- Tidy_Tuesday_3/scripts/tidy_script_3.R
+++ Tidy_Tuesday_3/scripts/tidy_script_3.R
@@ -5,7 +5,7 @@
 ### Load Libraries ###
 library(tidyverse)
 library(here)
-library(beyonce)
+library(rcartocolor)
 library(showtext)
 library(ggplot2)
 library(dplyr)
@@ -24,26 +24,34 @@ erasmus %>%
   count(academic_year, sort = TRUE) %>% 
   arrange(desc(academic_year))
 
+erasmus %>%
+  count(participant_nationality, sort = TRUE)
+
 eu_student <- erasmus %>%
-  filter(academic_year > 2018) %>% #selects 2018 and on
-  select(project_reference, academic_year, participant_nationality, participant_gender, participant_age) #selects variables
+  filter(academic_year > 2018, #selects 2018 and on
+         participant_gender != "Undefined", #removes undefined
+         participant_nationality %in% c("FR","UK","IE","SE","DE", "ES", "IT")) %>% #selects countries
+  select(project_reference, academic_year, participant_nationality, receiving_organization, participant_gender, participant_age) #selects variables
 view(eu_student)
 
 ### Plotting the data ###
 eu_student %>%
-  ggplot(aes(x = academic_year,
-             y = participant_age,
+  ggplot(aes(x = participant_nationality,
+             y = participant_age, 
              fill = participant_nationality))+
-  geom_boxplot()+
-  facet_wrap(~ participant_gender)+
-  labs(x = "Academic Year", #defines x axis
+  geom_bar(stat = "identity")+
+  coord_polar(theta = "x",start=0)+
+  facet_grid(academic_year ~ participant_gender)+ #creates sections
+  labs(x = "Participant Nationality", #defines x axis
        y = "Participant Age", #defines y axis
-       title = "Erasmus Participant Ages Between 2018-2020", #adds title
+       title = "Erasmus Participant Demographics Between 2018-2020", #adds title
+       fill = "Participant Nationality", #changes legent title
        caption = "Source: 2022-03-08 TidyTuesday Data Provided By Data.Europa hattip to Data is Plural")+ #adds caption
-  guides(fill = FALSE)+
   theme_bw()+
   theme(axis.title = element_text(size = 15), #changes axis title size
         plot.title = element_text(size = 20), #changes title size
-        text = element_text(family = "baskerville")) #adds font
+        text = element_text(family = "baskerville"))+ #adds font
+  scale_color_carto_d(name = "species", palette = "Safe") #adds color to fill
+
 
 ggsave(here('Tidy_Tuesday_3/output',"eu_student.png"), width = 7, height = 6) #exports plot
